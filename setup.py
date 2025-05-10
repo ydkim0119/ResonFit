d51ec4006f4a3dcc80fd8ed551a70dfd9d22a9c1
ydkim0119 <95066855+ydkim0119@users.noreply.github.com>
2025-05-10 19:19:57 +0900

    Fix encoding issue in setup.py for Windows compatibility

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+import io
 
 setup(
     name="resonfit",
@@ -22,7 +23,7 @@ setup(
     author="ydkim0119",
     author_email="youremail@example.com",  # Update with your email
     description="A modular tool for fitting superconducting resonator data",
-    long_description=open("README.md").read(),
+    long_description=io.open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/ydkim0119/ResonFit",
     classifiers=[
