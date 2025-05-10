e16a041903e47f344ecdc35ad9fd1334ef5d780c
ydkim0119 <95066855+ydkim0119@users.noreply.github.com>
2025-05-10 19:05:57 +0900

    Add visualization module and __init__.py file

---
+++ resonfit/visualization/__init__.py
@@ -0,0 +1,12 @@
+"""
+Visualization tools for resonator fitting and analysis.
+
+This module provides functions and classes for plotting and visualizing
+resonator data at different stages of preprocessing and fitting.
+"""
+
+from .plotter import ResonancePlotter
+
+__all__ = [
+    'ResonancePlotter',
+]
